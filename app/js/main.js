40045e1832e841c90c1d8a2876ff8c2ca5646879
Gabor Szabo <szabogabi@gmail.com>
2015-04-07 19:17:42 +0200

    custom font added

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$('.image-popup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile mfp-with-zoom',
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+		
+	});
+
+});
